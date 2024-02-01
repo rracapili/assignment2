a15912d75c120e686288d189058dc8d18e191041
rracapili <rracapili@gmail.com>
2024-02-01 14:20:34 -0500

    first commit

---
+++ modules/collegeData.js
@@ -0,0 +1,18 @@
+/*********************************************************************************
+*  WEB700 – Assignment 2
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Ronald Capili Student ID: radcapili Date: January 30, 2023
+*  GitHub Link: 
+********************************************************************************/ 
+
+class Data {
+    constructor(students, courses) {
+        this.students = students;
+        this.courses = courses;
+    }
+}
+
+var dataCollection = null
