d9a3a358fecfea3d59e240aae02661ae9823b7c3
rracapili <rracapili@gmail.com>
2024-02-03 20:58:47 -0500

    Assignment 2 - additional files

--- modules/collegeData.js
+++ modules/collegeData.js
@@ -4,9 +4,10 @@
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
-*  Name: Ronald Capili Student ID: radcapili Date: January 30, 2023
-*  GitHub Link: 
+*  Name: Ronald Aldrin Capili Student ID: radcapili Date: 2024/02/02
+*
 ********************************************************************************/ 
+
 
 class Data {
     constructor(students, courses) {
@@ -16,3 +17,89 @@ class Data {
 }
 
 var dataCollection = null
+
+
+const fs = require('fs');
+
+function initialize() {
+    return new Promise((resolve, reject) => {
+        // Read students.json
+        fs.readFile('./data/students.json', 'utf8', (err, studentDataFromFile) => {
+            if (err) {
+                reject("Unable to read students.json");
+                return;
+            }
+            // Read courses.json
+            fs.readFile('./data/courses.json', 'utf8', (err, courseDataFromFile) => {
+                if (err) {
+                    reject("Unable to read courses.json");
+                    return;
+                }
+                try {
+                    // Parse JSON data from files
+                    const students = JSON.parse(studentDataFromFile);
+                    const courses = JSON.parse(courseDataFromFile);
+
+                    // Create Data instance
+                    const dataCollection = new Data(students, courses);
+
+                    // Resolve the promise with the Data instance
+                    resolve(dataCollection);
+                } catch (parseError) {
+                    // If JSON parsing fails, reject the promise
+                    reject("Error parsing JSON data");
+                }
+            });
+        });
+    });
+}
+
+
+function getAllStudents(dataCollection) {
+    return new Promise((resolve, reject) => {
+        const students = dataCollection.students;
+
+        if (students.length === 0) {
+            reject("No results returned");
+        }
+        else {
+            resolve(students);
+        }
+    });
+}
+
+function getTAs(dataCollection) {
+    return new Promise((resolve, reject) => {
+        const students = dataCollection.students;
+        const tas = students.filter((student) => student.TA === true);
+
+        if (tas.length === 0) {
+            reject("No results returned");
+        }
+        else {
+            resolve(tas);
+        }
+    });
+}
+
+function getCourses(dataCollection) {
+    return new Promise((resolve, reject) => {
+        const courses = dataCollection.courses;
+
+        if (courses.length === 0) {
+            reject("No results returned");
+        }
+        else {
+            resolve(courses);
+        }
+    });
+}
+
+
+module.exports = {
+    initialize,
+    getAllStudents,
+    getTAs,
+    getCourses
+    // Other functions...
+};
